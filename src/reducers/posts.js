2996dc60044c8d78357e50dc985ebde66cc0e8ef
Mohit-Phuldaliya <phuldaliyamohit@gmail.com>
2022-10-18 20:27:28 +0530

    Added Store

---
+++ src/reducers/posts.js
@@ -0,0 +1,5 @@
+export default function posts(state = []) {
+  // our current state looks like :-
+  // { post : [] } array of psot
+  return state;
+}
---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import reducer from "../reducers/index"; //importing combineRducers from index.js file from reducers folder
+
+let store;
+
+export function configureStore() {
+  store = createStore(reducer, applyMiddleware(thunk, logger));
+
+  return store;
+}
